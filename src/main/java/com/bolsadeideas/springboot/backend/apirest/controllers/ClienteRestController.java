76d53990aa51dc4d9b83f20d42c6c54a4c355b25
Martín <magadiflo@gmail.com>
2022-05-02 11:46:29 -0500

    Uso de Cors para compartir recursos en API REST

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteServi
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class ClienteRestController {
 
 	@Autowired
