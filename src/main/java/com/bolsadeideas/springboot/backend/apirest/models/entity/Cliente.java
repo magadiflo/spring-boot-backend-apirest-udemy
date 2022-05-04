ffa05d465dd5b2aaeef445e070dabec589ac660c
Martín <magadiflo@gmail.com>
2022-05-04 12:30:26 -0500

    Añadiendo el campo para la fecha

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -8,12 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -39,14 +39,10 @@ public class Cliente implements Serializable {
 	@Column(nullable = false, unique = true)
 	private String email;
 
-	@Column(name = "create_at")
+	@NotNull
 	@Temporal(TemporalType.DATE)
+	@Column(name = "create_at")
 	private Date createAt;
-
-	@PrePersist
-	public void prePersist() {
-		this.createAt = new Date();
-	}
 
 	public Long getId() {
 		return id;
