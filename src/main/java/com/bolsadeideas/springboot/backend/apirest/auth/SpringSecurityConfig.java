50a01e3d4c205581878917aac252ee278462c34d
Martín <magadiflo@gmail.com>
2022-05-06 23:39:25 -0500

    Añadiendo la configuración para el servidor de autorización

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/auth/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/auth/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.backend.apirest.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +24,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(this.usuarioService).passwordEncoder(SpringSecurityConfig.passwordEncoder());
+	}
+
+	@Bean("authenticationManager")
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 
 }
