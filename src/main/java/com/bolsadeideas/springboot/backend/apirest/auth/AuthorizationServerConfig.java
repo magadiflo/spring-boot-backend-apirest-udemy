93989e8831039e4b80b2a94b74e054b15d168e05
Martín <magadiflo@gmail.com>
2022-05-10 11:22:09 -0500

    Añadiendo configuración de las aplicaciones clientes y acceso a endpoints

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/auth/AuthorizationServerConfig.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/auth/AuthorizationServerConfig.java
@@ -28,12 +28,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		super.configure(security);
+		security.tokenKeyAccess("permitAll()") //Genera el token cuando se autentica
+			.checkTokenAccess("isAuthenticated()"); //Valida el token que se está enviando
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		super.configure(clients);
+		clients.inMemory()
+			.withClient("angularapp")
+			.secret(this.passwordEnconder
+			.encode("12345"))
+			.scopes("read", "write")
+			.authorizedGrantTypes("password", "refresh_token")
+			.accessTokenValiditySeconds(3600)
+			.refreshTokenValiditySeconds(3600);
+
 	}
 
 	@Override
