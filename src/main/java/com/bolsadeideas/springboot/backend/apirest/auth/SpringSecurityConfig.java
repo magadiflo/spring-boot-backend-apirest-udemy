77ee1fa402a5df77b63145280b882b03c2700f58
Martín <magadiflo@gmail.com>
2022-05-10 11:40:58 -0500

    Añadiendo la configuración para el servidor de recurso

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/auth/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/auth/SpringSecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -31,5 +33,16 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
+	
+	//Por el lado de Spring
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.csrf().disable() //csrf, protege con un token los formularios, pero no lo necesitamos porque trabajamos con Angular por separado
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //Deshabilitamos el manejo de sessión en la autenticación por el lado de Spring Security ya que trabajamos con API REST por Token
+	}
+
 
 }
